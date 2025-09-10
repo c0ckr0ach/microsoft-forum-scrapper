58a39b67d5ebf4bd28e73c0b35004df75dfbed24
hrishi <hrishim452@gmail.com>
2025-09-10 18:19:52 +0530

    Initial commit

---
+++ requests_tutorial.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+
+def extract_topic_list(soup):
+    topic_links = []
+
+
+    for topic in soup.find_all('h2', class_ = 'title is-6 margin-bottom-xxs'):
+        topic_links.append('https://learn.microsoft.com' + topic.a['href'])
+
+    return topic_links
+
+
+def extract_question_details(question_link):
+    thread = requests.get(question_link)
+
+    with open('htmls/thread.html', 'w', encoding='utf-8') as f:
+        f.write(thread.text)
+
+    with open('htmls/thread.html', 'r', encoding='utf-8') as f:
+        thread = f.read()
+
+    soup = BeautifulSoup(thread, 'lxml')
+
+    question_id = re.search(r'/questions/(\d+)/', question_link).group(1)
+
+    title = soup.find('h1', id= "answers").text
+
+    description = soup.find('div', class_="content margin-top-xxs").text
+    
+    answers_list = []
+    answers = soup.find('div', class_="margin-top-xxs")
+    print(answers)
+    
+    return {'event_id': question_id,'title': title, 'description': description, 'answers': answers_list} 
+
+
+
+
+site = requests.get('https://learn.microsoft.com/en-us/answers/tags/824/windows-home')
+# create a folder named htmls in the current directory before running this code
+with open('htmls/index.html', 'w', encoding='utf-8') as f:
+    f.write(site.text) 
+
+with open('htmls/index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+soup = BeautifulSoup(content, 'lxml')
+
+
+
+data = extract_topic_list(soup)
+with open('output_files/links.json', 'w', encoding='utf-8') as file:
+    for item in data:
+        json.dump(item, file)
+        file.write('\n')
+
+# details = extract_question_details(data[8])
+# print(details)
+print('end---')
+print( )
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+if expandable_section:
+        #     comment_list = expandable_section.find_all('li', class_ ="is-unstyled")
+
+        #     for comment in comment_list:
+        #         comment_text = comment.find('div', class_= 'content font-size-sm')
+        #         comments.append(comment_text.text)
